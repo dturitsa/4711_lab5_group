adf3e032958c11103fe586e094d75dd1f9019deb
Duy Pham <duy.phamthe91@gmail.com>
2016-02-21 15:49:37 -0800

    Fixed order menu
    Signed-off-by: Duy Pham <duy.phamthe91@gmail.com>

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -2,86 +2,104 @@
 
 /**
  * Order handler
- * 
+ *
  * Implement the different order handling usecases.
- * 
+ *
  * controllers/welcome.php
  *
  * ------------------------------------------------------------------------
  */
-class Order extends Application {
+class Order extends Application
+{
 
-    function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
     // start a new order
-    function neworder() {
-        $order_num = $this->orders->highest() + 1;
-        $neworder = $this->orders->create();
-        $neworder->num = $order_num;
-        $neworder->date = date();
+    public function neworder()
+    {
+        $order_num        = $this->orders->highest() + 1;
+
+        $neworder         = $this->orders->create();
+        
+        $neworder->num    = $order_num;
+        
+        $neworder->date   = date();
+        
         $neworder->status = 'a';
-        $neworder->total = 0;
+        
+        $neworder->total  = 0;
+
+        $this->orders->add($neworder);
+        
         redirect('/order/display_menu/' . $order_num);
     }
 
     // add to an order
-    function display_menu($order_num = null) {
-        
-        if ($order_num == null)
-            redirect('/order/neworder');
+    public function display_menu($order_num = null)
+    {
 
-        $this->data['pagebody'] = 'show_menu';
+        if ($order_num == null) {
+            redirect('/order/neworder');
+        }
+
+        $this->data['pagebody']  = 'show_menu';
         $this->data['order_num'] = $order_num;
-        //FIXME
+        $this->data['title'] = 'Order #' . $order_num;
 
         // Make the columns
-        $this->data['meals'] = $this->make_column('m');
+        $this->data['meals']  = $this->make_column('m');
         $this->data['drinks'] = $this->make_column('d');
         $this->data['sweets'] = $this->make_column('s');
 
-	// Bit of a hokey patch here, to work around the problem of the template
-	// parser no longer allowing access to a parent variable inside a
-	// child loop - used for the columns in the menu display.
-	// this feature, formerly in CI2.2, was removed in CI3 because
-	// it presented a security vulnerability.
-	// 
-	// This means that we cannot reference order_num inside of any of the
-	// variable pair loops in our view, but must instead make sure
-	// that any such substitutions we wish make are injected into the 
-	// variable parameters
-	// Merge this fix into your origin/master for the lab!
-	$this->hokeyfix($this->data['meals'],$order_num);
-	$this->hokeyfix($this->data['drinks'],$order_num);
-	$this->hokeyfix($this->data['sweets'],$order_num);
-	// end of hokey patch
-	
+        // Bit of a hokey patch here, to work around the problem of the template
+        // parser no longer allowing access to a parent variable inside a
+        // child loop - used for the columns in the menu display.
+        // this feature, formerly in CI2.2, was removed in CI3 because
+        // it presented a security vulnerability.
+        //
+        // This means that we cannot reference order_num inside of any of the
+        // variable pair loops in our view, but must instead make sure
+        // that any such substitutions we wish make are injected into the
+        // variable parameters
+        // Merge this fix into your origin/master for the lab!
+        $this->hokeyfix($this->data['meals'], $order_num);
+        $this->hokeyfix($this->data['drinks'], $order_num);
+        $this->hokeyfix($this->data['sweets'], $order_num);
+        // end of hokey patch
+
         $this->render();
     }
 
     // inject order # into nested variable pair parameters
-    function hokeyfix($varpair,$order) {
-	foreach($varpair as &$record)
-	    $record->order_num = $order;
+    public function hokeyfix($varpair, $order)
+    {
+        foreach ($varpair as &$record) {
+            $record->order_num = $order;
+        }
+
     }
-    
+
     // make a menu ordering column
-    function make_column($category) {
-        //FIXME
-        return $items;
+    public function make_column($category)
+    {
+        return $this->menu->some('category', $category);
     }
 
     // add an item to an order
-    function add($order_num, $item) {
+    public function add($order_num, $item)
+    {
         //FIXME
         redirect('/order/display_menu/' . $order_num);
     }
 
     // checkout
-    function checkout($order_num) {
-        $this->data['title'] = 'Checking Out';
-        $this->data['pagebody'] = 'show_order';
+    public function checkout($order_num)
+    {
+        $this->data['title']     = 'Checking Out';
+        $this->data['pagebody']  = 'show_order';
         $this->data['order_num'] = $order_num;
         //FIXME
 
@@ -89,13 +107,15 @@ class Order extends Application {
     }
 
     // proceed with checkout
-    function commit($order_num) {
+    public function commit($order_num)
+    {
         //FIXME
         redirect('/');
     }
 
     // cancel the order
-    function cancel($order_num) {
+    public function cancel($order_num)
+    {
         //FIXME
         redirect('/');
     }
