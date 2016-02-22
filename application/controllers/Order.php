72150e3ea2b6fc30a5235ba0bb615ea5c94563c4
Duy Pham <duy.phamthe91@gmail.com>
2016-02-21 16:59:10 -0800

    Fixed task 7: Finish commit and cancel method. Fixed flush method in Orders model
    Signed-off-by: Duy Pham <duy.phamthe91@gmail.com>

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -101,22 +101,22 @@ class Order extends Application
     // checkout
     public function checkout($order_num)
     {
-        $this->data['title']     = 'Checking Out';
-        
-        $this->data['pagebody']  = 'show_order';
-        
+        $this->data['title'] = 'Checking Out';
+
+        $this->data['pagebody'] = 'show_order';
+
         $this->data['order_num'] = $order_num;
-        
+
         $this->data['total'] = '$' . number_format($this->orders->total($order_num), 2);
-        
+
         $this->data['items'] = $this->orders->details($order_num);
-    
+
         $items = $this->orderitems->group($order_num);
-        foreach($items as $item){
-            $menuitem = $this->menu->get($item->item);
+        foreach ($items as $item) {
+            $menuitem   = $this->menu->get($item->item);
             $item->code = $menuitem->name;
         }
-        $this->data['items'] = $items;
+        $this->data['items']   = $items;
         $this->data['okornot'] = $this->orders->validate($order_num);
         $this->render();
     }
@@ -124,14 +124,36 @@ class Order extends Application
     // proceed with checkout
     public function commit($order_num)
     {
-        //FIXME
+        if ($this->orders->validate($order_num)) {
+            redirect('/order/display_menu/' . $order_num);
+        }
+
+        $record = $this->orders->get($order_num);
+
+        $record->date = date(DATE_ATOM);
+
+        $record->status = 'c';
+
+        $record->total = $this->orders->total($order_num);
+
+        $this->orders->update($record);
+
+        $this->orders->flush($order_num);
+
         redirect('/');
     }
 
     // cancel the order
     public function cancel($order_num)
     {
-        //FIXME
+        $this->orderitems->delete_some($order_num);
+
+        $record = $this->orders->get($order_num);
+
+        $record->status = 'x';
+
+        $this->orders->update($record);
+
         redirect('/');
     }
 
